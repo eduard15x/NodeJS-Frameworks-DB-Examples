4f7f75703926666231994a95b9aa046b4dc8a9d7
Precup Eduard <precupeduard99@gmail.com>
2022-11-06 23:42:12 +0200

    Get deep into DB -> MongoDB, connection, mongoose

--- backend/NASA-PROJECT/server/src/server.js
+++ backend/NASA-PROJECT/server/src/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
@@ -6,9 +7,22 @@ const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = 'mongodb+srv://nasa-api:38cLbMrABYlKiPpX@nasacluster.bxzfqjk.mongodb.net/?retryWrites=true&w=majority'
+
 const server = http.createServer(app);
 
+//check if connection is working/is ready/succeded
+
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+});
+
 async function startServer() {
+    await mongoose.connect(MONGO_URL);
 
     await loadPlanetsData();
 
